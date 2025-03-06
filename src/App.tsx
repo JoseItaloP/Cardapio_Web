ee94ce9f47cb45b17b11111f96e48d26defb4b8e
Jose Italo <122283414+JoseItaloP@users.noreply.github.com>
2025-03-06 10:13:58 -0300

    Criaçao dos elementos de css iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import "./App.css";
 import Footer from "./components/Footer";
-import Header from "./components/header";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
       <main className="MainCardapioPage">
-        <h1>Tittle of Restaurant web page</h1>
+        <h1>Cardapio Web</h1>
+        <div className="DivPagePedido">
+
+          <section className="ConjuntoTipoPratos">
+            <h1>Conjunto de tipos de pratos</h1>  
+            <ul className="ListaDPratos">
+              <li className="CaixaPrato">
+                <h2>Nome do prato</h2>
+                <p>Descrição do prato</p>
+                <strong>Valor do prato</strong>
+                <button>Adicionar ao carinho</button>
+              </li>
+            </ul>
+          </section>
+
+          <button className="ButaoConclusaoP">Conclusão de pedido</button>
+
+        </div>
       </main>
       <Footer />
     </>
